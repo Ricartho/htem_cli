ef9b2fb2b7ef2a2f31d2a8eb3725546a6366eb34
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-14 14:30:01 +0000

    premier pas

--- lib/htem_cli.rb
+++
@@ -1,6 +0,0 @@
-require "htem_cli/version"
-
-module HtemCli
-  class Error < StandardError; end
-  # Your code goes here...
-end
---
+++ lib/htem_cli_relatives.rb
@@ -0,0 +1,7 @@
+#contain all the relations between files
+require 'bundler/setup'
+Bundler.require(:default)
+require_relative "./htem_cli/version"
+require_relative "./htem_cli/cli"
+require_relative "./htem_cli/scraper"
+require_relative "./htem_cli/article"
